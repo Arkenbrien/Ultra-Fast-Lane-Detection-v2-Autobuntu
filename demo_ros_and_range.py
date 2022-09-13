3371d06137677b1646df6ca7065a4ae5a58eb806
TravisMoleski <tm769613@ohio.edu>
2022-09-13 15:47:28 -0400

    added lane range and width

--- demo_ros_and_range.py
+++ demo_ros_and_range.py
@@ -1,3 +1,4 @@
+from cmath import isnan
 import torch, os, cv2
 from utils.dist_utils import dist_print
 import torch, os
@@ -22,6 +23,61 @@ class pixel_range_sub(object):
 
 def distance(point1,point2):
     return ((point1[0] - point2[0])**2 + (point1[1]-point2[1])**2)**(1/2)
+
+
+def extract_lane_ranges(line_list, pixel_points, pixel_threshold):
+    lidar_point_master = []
+    for point in pixel_points.points:
+        pixel_u = point.u
+        pixel_v = point.v
+
+        pixel_r = point.range
+        pixel_x = point.x
+        pixel_y = point.y
+        pixel_z = point.z
+        color = (255,0,0)
+
+        for line in range(len(line_list)):
+            for point_px in range(len(line_list[line])):
+                line_pix = line_list[line][point_px]
+                distance_px = distance([pixel_u, pixel_v], line_pix)
+                if distance_px < pixel_threshold:
+                    lidar_point_master.append([pixel_x, pixel_y, pixel_z, pixel_u, pixel_v, line])
+            
+    return_list = []
+    for line_number in range(len(line_list)):
+        this_line_x  = []
+        this_line_y  = []
+        this_line_z  = []
+        this_line_u  = []
+        this_line_v  = []
+
+        for matched_point in range(len(lidar_point_master)):
+            extracted_point = lidar_point_master[matched_point]
+            pixel_x =  extracted_point[0]
+            pixel_y =  extracted_point[1]
+            pixel_z =  extracted_point[2]
+            pixel_u =  extracted_point[3]
+            pixel_v =  extracted_point[4]
+            line    =  extracted_point[5]
+
+            if line == line_number:
+                color = (255,0,255)
+                this_line_x.append(pixel_x)
+                this_line_y.append(pixel_y)
+                this_line_z.append(pixel_z)
+                this_line_u.append(pixel_u)
+                this_line_v.append(pixel_v)
+
+        line_x_avg = np.average(this_line_x)
+        line_y_avg = np.average(this_line_y)
+        line_z_avg = np.average(this_line_z)
+        line_u_avg = np.average(this_line_u)
+        line_v_avg = np.average(this_line_v)
+
+        return_list.append([line_x_avg, line_y_avg, line_z_avg, line_u_avg, line_v_avg, line])
+
+    return return_list
 
 def pred2coords(pred, row_anchor, col_anchor, local_width = 1, original_image_width = 1640, original_image_height = 590):
     batch_size, num_grid_row, num_cls_row, num_lane_row = pred['loc_row'].shape
@@ -71,6 +127,7 @@ def pred2coords(pred, row_anchor, col_anchor, local_width = 1, original_image_wi
     
 if __name__ == "__main__":
     torch.backends.cudnn.benchmark = True
+    
 
     rospy.init_node('ultra_fast_v2', anonymous=True)
 
@@ -81,6 +138,8 @@ if __name__ == "__main__":
     range_lane = False
 
     rospy.wait_for_message(cfg.ros_topic, Image, timeout=60)
+    pixel_range_dat = pixel_range_sub()
+    pixel_threshold = 10
     time.sleep(1)
 
     dist_print('start testing...')
@@ -117,6 +176,9 @@ if __name__ == "__main__":
         img_h = datasets[0].cv_image.shape[0]
         extracted_image = datasets[0].cv_image
 
+        pixel_info = pixel_range_dat.pixel_vector
+        extracted_image = datasets[0].cv_image
+
         for split, dataset in zip(splits, datasets):
             loader = torch.utils.data.DataLoader(dataset, batch_size=1, shuffle = False, num_workers=1)
             for i, data in enumerate(loader):
@@ -127,12 +189,46 @@ if __name__ == "__main__":
 
                 coords = pred2coords(pred, cfg.row_anchor, cfg.col_anchor, original_image_width = img_w, original_image_height = img_h)
                 count = 0
+
+                line_list = []
                 for lane in coords:
+                    line = []
                     if count < 2:
                         for coord in lane:
+                            line.append(coord)
                             cv2.circle(extracted_image, coord, 5, (0,255,0), -1)
+                    line_list.append(line)
                     count += 1
+            
+                lane_range_extract = extract_lane_ranges(line_list, pixel_info, pixel_threshold)
+                
+                dev_list = []
+                for results in lane_range_extract:
+                    line_x_avg = results[0]
+                    line_y_avg = results[1]
+                    line_z_avg = results[2]
+                    line_u_avg = results[3]
+                    line_v_avg = results[4]
+                    count = results[5]
 
+                    if not np.isnan(line_x_avg):
+                        dev_list.append(line_x_avg)
+
+                    try:
+                        cv2.putText(extracted_image, str(count)+" DEV: "+str(round(line_x_avg,2)), (int(line_u_avg+30), int(line_v_avg)), cv2.FONT_HERSHEY_SIMPLEX, 2, (255,255,255), 3, cv2.LINE_AA)
+                    except:
+                        continue
+
+                lane_min = np.min(dev_list)
+                lane_max = np.max(dev_list)
+                lane_width = np.abs(lane_min) + np.abs(lane_max)
+                try:
+                    cv2.putText(extracted_image, "Lane Width:  "+str(round(lane_width,2))+"m", (int(img_w/2), int(img_h-img_h*0.02)), cv2.FONT_HERSHEY_SIMPLEX, 2, (0,0,0), 5, cv2.LINE_AA)
+                except:
+                    continue
+                
+
+            
         vis = cv2.resize(extracted_image, (640,480))
         cv2.imshow('vis',vis)
         cv2.waitKey(1)
--- launch_demo.sh
+++ launch_demo.sh
@@ -4,4 +4,4 @@
 
 # Travis
 # python3 demo.py configs/culane.py --test_model weights/culane_18.pth --data_root /home/autobuntu/Documents/Ultra-Fast-Lane-Detection --dataset 'Live' 
-python3 demo_ros.py configs/tusimple_res18.py --test_model weights/tusimple_res18.pth --data_root /home/travis/lane_detection/Ultra-Fast-Lane-Detection-v2-Autobuntu --dataset 'Tusimple' --ros_topic /camera_fl/image_raw_rgb
+python3 demo_ros_and_range.py configs/culane_res34.py --test_model weights/culane_res34.pth --data_root /home/travis/lane_detection/Ultra-Fast-Lane-Detection-v2-Autobuntu --ros_topic /airsim_node/CAR1/front_center/Scene_bgr_opencv
